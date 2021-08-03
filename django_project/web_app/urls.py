18499601769785ceb7e92ae711af0e040cb2deff
Ivan Chepets <i.chepets@quantumobile.com>
2021-08-03 16:39:13 +0300

    Add function

--- django_project/web_app/urls.py
+++ django_project/web_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
-#URLConf
+# URLConf
 urlpatterns = [
-    path('hello/', views.say_hello)
+    path('', views.home, name='home'),
+    path('add', views.add, name='add')
 ]
