dd82ff68ee1c60209a69969943246c7e5e3ad20c
i.chepets <i.chepets@quantumobile.com>
2021-09-08 16:52:58 +0300

    in progress

--- tasks.py
+++ tasks.py
@@ -2,10 +2,13 @@ from invoke import task
 
 
 @task
-def build(c):
+def build(c): #run_local
     c.run(
-        "PGPASSWORD=postgres dropdb -h db -U postgres -W django_db &&"
+        "PGPASSWORD=postgres dropdb -h db -U postgres -W django_db &&" #add to script
         "PGPASSWORD=postgres createdb -h db -U postgres django_db &&"
         "python3 ./django_project/manage.py dbshell < db_dump.sql"
+
         # "python3 ./django_project/manage.py migrate web_app"
+        # collect static
     )
+    # runserver
