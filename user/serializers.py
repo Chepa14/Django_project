cae759c6655d6b6b7fd985f53680f9d01dfe2139
i.chepets <i.chepets@quantumobile.com>
2021-09-29 10:29:57 +0300

    Fixed UserSerializer

--- user/serializers.py
+++ user/serializers.py
@@ -1,11 +1,9 @@
 from rest_framework.serializers import ModelSerializer
 from django.contrib.auth import get_user_model
-from user.models import User
 
 
 class UserSerializer(ModelSerializer):
     class Meta:
-        # model = get_user_model()
-        model = User
+        model = get_user_model()
         fields = ("username", "email", "first_name", "last_name", "about_me", "avatar")
         read_only_fields = ("username", "email")
