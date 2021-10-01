c1257a33a024e0bac29ebe5de95823dfe2d4a2f6
i.chepets <i.chepets@quantumobile.com>
2021-10-01 16:54:38 +0300

    Endpoints fixed

--- artist/models.py
+++ artist/models.py
@@ -1,12 +1,41 @@
 from django.db import models
+import os
+
+
+def get_song_upload_path(instance, filename):
+    print(instance)
+    return os.path.join(
+        instance.author.pseudonym,
+        instance.file_path,
+        filename
+    )
+
+
+def get_artist_upload_path(instance, filename):
+    print(instance)
+    return os.path.join(
+        instance.pseudonym,
+        instance.file_path,
+        filename
+    )
 
 
 class Artist(models.Model):
+    pseudonym = models.CharField(max_length=20, default='')
+    first_name = models.CharField(max_length=20, default='', blank=True)
+    last_name = models.CharField(max_length=30, default='', blank=True)
+    image = models.ImageField(upload_to=get_artist_upload_path, blank=True)
+    description = models.TextField(blank=True)
 
-    name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='imgs')
-    description = models.TextField()
-    welcome_str = models.CharField(max_length=100, default='Hi')
+    def __str__(self):
+        return self.pseudonym
+
+
+class Song(models.Model):
+    name = models.CharField(max_length=30)
+    author = models.ManyToManyField(Artist, default=[None], blank=True)
+    image = models.ImageField(upload_to=get_song_upload_path, default=None, blank=True)
+    time_length_sec = models.IntegerField(default=0, blank=True)
 
     def __str__(self):
         return self.name
--- artist/urls.py
+++ artist/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ArtistView, ArtistList
+from .views import ArtistView, ArtistList, SongView, SongsList
 
 urlpatterns = [
     path('<int:id>/', ArtistView.as_view(), name='artist'),
     path('', ArtistList.as_view(), name='artist-list'),
+    path('<int:artist_id>/songs/', SongsList.as_view(), name='songs-list'),
+    path('<int:artist_id>/songs/<int:song_id>/', SongView.as_view(), name='songs')
 ]
