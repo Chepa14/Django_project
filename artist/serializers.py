82bb670937878f6144426ad1a5eb9581db403f0f
i.chepets <i.chepets@quantumobile.com>
2021-10-01 16:54:38 +0300

    Endpoints fixed

--- artist/serializers.py
+++ artist/serializers.py
@@ -1,7 +1,29 @@
 from rest_framework import serializers
+from .models import Song, Artist
 
 
 class ArtistSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=100)
+    pseudonym = serializers.CharField(max_length=20)
+    first_name = serializers.CharField(max_length=20)
+    last_name = serializers.CharField(max_length=30)
     image = serializers.ImageField()
     description = serializers.CharField()
+
+
+class SongSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=30)
+    image = serializers.ImageField(allow_null=True)
+    author = ArtistSerializer(many=True, allow_null=True)
+    time_length_sec = serializers.IntegerField(allow_null=True)
+
+    class Meta:
+        model = Song
+        fields = ('author', 'name', 'image', 'time_length_sec')
+        read_only_fields = ('author',)
+
+    def create(self, validated_data):
+        validated_data.pop('author')
+        song = Song.objects.create(**validated_data)
+        current_author = Artist.objects.get(pk=self.context['artist_id'])
+        song.author.add(current_author)
+        return song
--- artist/views.py
+++ artist/views.py
@@ -1,7 +1,7 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from .serializers import ArtistSerializer
-from .models import Artist
+from .serializers import ArtistSerializer, SongSerializer
+from .models import Artist, Song
 
 
 class ArtistView(RetrieveUpdateDestroyAPIView):
@@ -15,3 +15,22 @@ class ArtistList(ListCreateAPIView):
     permission_classes = (IsAuthenticatedOrReadOnly,)
     serializer_class = ArtistSerializer
     queryset = Artist.objects.all()
+
+
+class SongView(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    serializer_class = SongSerializer
+
+    def get_object(self):
+        return Song.objects.filter(author__id=self.kwargs['artist_id']).get(pk=self.kwargs['song_id'])
+
+
+class SongsList(ListCreateAPIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    serializer_class = SongSerializer
+
+    def get_queryset(self):
+        return Song.objects.filter(author__id=self.kwargs['artist_id'])
+
+    def get_serializer_context(self):
+        return {"artist_id": self.kwargs['artist_id']}
