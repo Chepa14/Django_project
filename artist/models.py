084e16838cc562ee2967cc47533ed2ff4191854a
i.chepets <i.chepets@quantumobile.com>
2021-10-04 14:49:29 +0300

    Artist app extended

--- artist/models.py
+++ artist/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from user.models import User
 import os
 
 
@@ -26,9 +27,23 @@ class Artist(models.Model):
     last_name = models.CharField(max_length=30, default='', blank=True)
     image = models.ImageField(upload_to=get_artist_upload_path, blank=True)
     description = models.TextField(blank=True)
+    likes = models.ManyToManyField(User, default=[None], blank=True)
+    likes_number = models.IntegerField(default=0, blank=True)
 
     def __str__(self):
         return self.pseudonym
+
+    @property
+    def number_of_likes(self):
+        return self.likes.all().count()
+
+    @number_of_likes.setter
+    def number_of_likes(self, value):
+        self.likes_number = value
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
 
 
 class Song(models.Model):
