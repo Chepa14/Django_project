82cfda446dcf71cfb1012ba3f6c1077c6f1bf0ac
i.chepets <i.chepets@quantumobile.com>
2022-01-04 22:24:25 +0200

    TimeZone fixed

--- user/middlewares.py
+++ user/middlewares.py
@@ -12,8 +12,9 @@ class TimezoneMiddleware(object):
         self.get_response = get_response
 
     def __call__(self, request):
-        tz_name = request.headers.get('Timezone', None)
-        tz = pytz.timezone(tz_name)
-        timezone.activate(tz)
+        if not timezone.get_current_timezone_name():
+            tz_name = request.headers.get('Timezone', None)
+            tz = pytz.timezone(tz_name)
+            timezone.activate(tz)
         response = self.get_response(request)
         return response
