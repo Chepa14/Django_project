cbecc3a2b75e465ace6c68982f14e9660ce254b6
i.chepets <i.chepets@quantumobile.com>
2022-03-18 14:31:40 +0200

    Fixed HomePage & AboutPage

---
+++ frontend/src/components/About.js
@@ -0,0 +1,24 @@
+import React, {Component} from "react";
+
+class About extends Component{
+    render() {
+        return(
+            <div id="al_screenshot" className="Albums">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="titlepage">
+                                <h2> A story about this project </h2>
+                                <span>It is a long established fact that a reader will be distracted by the readable
+                                    <br/>content of a page when looking at its layout. The point of using Lorem
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default About;
